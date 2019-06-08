cdf61e983c147a311f61c943e50e9ff12f8faf90
axel paulander <axel.paulander@gmail.com>
2019-06-08 15:58:15 +0200

    added Test(error)

--- noerr.go
+++ noerr.go
@@ -2,26 +2,31 @@ package noerr
 
 import "log"
 
+//Test Returns err == nil
+func Test(err error) bool {
+	return err == nil
+}
+
 //Log Returns wether or not the error is nil, logs the error if err != nil
 func Log(err error) bool {
-	if err != nil {
+	t := !Test(err)
+	if t {
 		log.Println(err)
-		return false
 	}
 
-	return true
+	return t
 }
 
 //Fatal Checks if the error is nil, if not log.Fatal(err)
 func Fatal(err error) {
-	if err != nil {
+	if !Test(err) {
 		log.Fatal(err)
 	}
 }
 
 //Panic Checks if the error is nil, if not panic(err)
 func Panic(err error) {
-	if err != nil {
+	if !Test(err) {
 		panic(err)
 	}
 }

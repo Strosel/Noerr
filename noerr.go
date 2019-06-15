4b02a52b02840526be5fccd59e482faad3cc5279
axel paulander <axel.paulander@gmail.com>
2019-06-15 16:21:46 +0200

    added LogS and FatalS

--- noerr.go
+++ noerr.go
@@ -17,10 +17,27 @@ func Log(err error) bool {
 	return t
 }
 
+//LogS Returns wether or not the error is nil, logs the error appended to msg if err != nil
+func LogS(msg string, err error) bool {
+	t := !Test(err)
+	if t {
+		log.Println(msg, err)
+	}
+
+	return t
+}
+
 //Fatal Checks if the error is nil, if not log.Fatal(err)
 func Fatal(err error) {
 	if !Test(err) {
 		log.Fatal(err)
+	}
+}
+
+//FatalS Checks if the error is nil, if not log.Fatal(msg, err)
+func FatalS(msg string, err error) {
+	if !Test(err) {
+		log.Fatal(msg, err)
 	}
 }
 
